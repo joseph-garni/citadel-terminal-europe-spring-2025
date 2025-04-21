dbe2c682a97ab0b17a4bb99fafd8288780d0da6b
Ansh Pincha <89136648+anshpincha@users.noreply.github.com>
2025-04-21 17:08:26 +0100

    Updated to repair walls

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -25,6 +25,12 @@ class AlgoStrategy(gamelib.AlgoCore):
         seed = random.randrange(maxsize)
         random.seed(seed)
         gamelib.debug_write('Random seed: {}'.format(seed))
+        
+        # Add these instance variables to track wall information
+        self.wall_breaches = []  # Walls that were breached in the current turn
+        self.low_health_walls = []  # Walls with health below 40%
+        self.wall_locations = []  # All wall locations
+        self.scored_on_locations = []
 
     def on_game_start(self, config):
         """ 
@@ -41,8 +47,15 @@ class AlgoStrategy(gamelib.AlgoCore):
         INTERCEPTOR = config["unitInformation"][5]["shorthand"]
         MP = 1
         SP = 0
-        # This is a good place to do initial setup
-        self.scored_on_locations = []
+        
+        # Initialize default wall locations - customize this for your strategy
+        self.default_wall_locations = [[0,13], [1,13], [2,13], [3,13], [4,13], [5,13], [6,13], 
+                                       [7,13], [8,13], [9,13], [10,13], [11,13], [12,13], [13,13], 
+                                       [14,13], [15,13], [16,13], [17,13], [18,13], [19,13], [20,13], 
+                                       [21,13], [22,13], [23,13], [24,13], [25,13], [26,13], [27,13],
+                                       [1,12], [2,12], [4,12], [5,12], [7,12], [8,12], [9,12], [10,12], [11,12],
+                                       [12,12], [13,12], [14,12], [15,12], [16,12], [17,12], [18,12], [19,12], [20,12], 
+                                       [22,12], [23,12], [25,12], [26,13]]
 
     def on_turn(self, turn_state):
         """
@@ -54,82 +67,173 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         game_state = gamelib.GameState(self.config, turn_state)
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        game_state.suppress_warnings(True)  # Comment or remove this line to enable warnings.
 
-        self.starter_strategy(game_state)
+        # Update our tracking variables at the beginning of each turn
+        self.wall_locations = self.get_wall_locations(game_state)
+        self.find_low_health_walls(game_state, 0.4)  # Find walls below 40% health
+        
+        # Print debug information
+        gamelib.debug_write(f"Wall breaches from last turn: {self.wall_breaches}")
+        gamelib.debug_write(f"Low health walls: {self.low_health_walls}")
+        
+        # Run our improved strategy
+        self.improved_strategy(game_state)
 
         game_state.submit_turn()
 
-
-    """
-    NOTE: All the methods after this point are part of the sample starter-algo
-    strategy and can safely be replaced for your custom algo.
-    """
-
-    def starter_strategy(self, game_state):
+    def get_wall_locations(self, game_state):
         """
-        For defense we will use a spread out layout and some interceptors early on.
-        We will place turrets near locations the opponent managed to score on.
-        For offense we will use long range demolishers if they place stationary units near the enemy's front.
-        If there are no stationary units to attack in the front, we will send Scouts to try and score quickly.
+        Get all locations where we have walls
+        
+        Args:
+            game_state: The current game state
+            
+        Returns:
+            A list of locations where we have walls
         """
-        # First, place basic defenses
-        self.build_defences(game_state)
-        # Now build reactive defenses based on where the enemy scored
-        self.build_reactive_defense(game_state)
+        wall_locations = []
+        
+        for x in range(game_state.ARENA_SIZE):
+            for y in range(game_state.HALF_ARENA):  # Only check our side of the map
+                location = [x, y]
+                if game_state.contains_stationary_unit(location):
+                    unit = game_state.contains_stationary_unit(location)
+                    if unit.unit_type == WALL and unit.player_index == 0:  # It's our wall
+                        wall_locations.append(location)
+        
+        return wall_locations
 
-        # If the turn is less than 5, stall with interceptors and wait to see enemy's base
-        if game_state.turn_number < 5:
-            self.stall_with_interceptors(game_state)
-        else:
-            # Now let's analyze the enemy base to see where their defenses are concentrated.
-            # If they have many units in the front we can build a line for our demolishers to attack them at long range.
-            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
-                self.demolisher_line_strategy(game_state)
-            else:
-                # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
+    def find_low_health_walls(self, game_state, health_threshold=0.4):
+        """
+        Find walls with health below the given threshold (default 40%)
+        
+        Args:
+            game_state: The current game state
+            health_threshold: The health threshold as a decimal (0.4 = 40%)
+            
+        Returns:
+            A list of locations of walls with health below the threshold
+        """
+        self.low_health_walls = []
+        
+        # Get all locations with walls
+        for x in range(game_state.ARENA_SIZE):
+            for y in range(game_state.HALF_ARENA):  # Only check our side of the map
+                location = [x, y]
+                if game_state.contains_stationary_unit(location):
+                    unit = game_state.contains_stationary_unit(location)
+                    if unit.unit_type == WALL and unit.player_index == 0:  # It's our wall
+                        # Get the maximum health of a wall
+                        max_health = gamelib.GameUnit(WALL, game_state.config).max_health
+                        
+                        # Check if health is below threshold
+                        if unit.health / max_health < health_threshold:
+                            self.low_health_walls.append(location)
+                            gamelib.debug_write(f"Low health wall at {location} with {unit.health}/{max_health} health")
+        
+        return self.low_health_walls
 
-                # Only spawn Scouts every other turn
-                # Sending more at once is better since attacks can only hit a single scout at a time
-                if game_state.turn_number % 2 == 1:
-                    # To simplify we will just check sending them from back left and right
-                    scout_spawn_location_options = [[13, 0], [14, 0]]
-                    best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
-                    game_state.attempt_spawn(SCOUT, best_location, 1000)
-
-                # Lastly, if we have spare SP, let's build some supports
-                support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
-                game_state.attempt_spawn(SUPPORT, support_locations)
-
+    def repair_walls(self, game_state):
+        """
+        Repair damaged walls and replace breached walls
+        """
+        # First, replace any breached walls from the previous turn
+        for location in self.wall_breaches:
+            if game_state.can_spawn(WALL, location):
+                game_state.attempt_spawn(WALL, location)
+                gamelib.debug_write(f"Repairing breached wall at {location}")
+        
+        # Then, repair low health walls
+        for location in self.low_health_walls:
+            # Check if the wall still exists (might have been destroyed completely)
+            if game_state.contains_stationary_unit(location):
+                # Remove and rebuild the wall
+                game_state.attempt_remove([location])
+                game_state.attempt_spawn(WALL, location)
+                gamelib.debug_write(f"Replacing low health wall at {location}")
+    
     def build_defences(self, game_state):
         """
         Build basic defenses using hardcoded locations.
         Remember to defend corners and avoid placing units in the front where enemy demolishers can attack them.
         """
-        # Useful tool for setting up your base locations: https://www.kevinbai.design/terminal-map-maker
-        # More community tools available at: https://terminal.c1games.com/rules#Download
-
         # Place turrets that attack enemy units
-        turret_locations = [[[3,12], [24,12],[21,12]]
+        turret_locations = [[3, 12], [24, 12], [21, 12], [6,12]]
         # attempt_spawn will try to spawn units if we have resources, and will check if a blocking unit is already there
         game_state.attempt_spawn(TURRET, turret_locations)
         
         # Place walls in front of turrets to soak up damage for them
-        wall_locations = [[0,13],[1,13], [2,13], [3,13], [4,13], [5,13], [6,13], [7,13], [8,13], [9,13], [10,13],[11,13], [12,13], [13,13], [14,13], [15,13], [16,13], [17,13], [18,13], [19,13], [20,13], [21,13], [22,13], [25,13], [26,13],[27,13]]
-        game_state.attempt_spawn(WALL, wall_locations)
-        # upgrade walls so they soak more damage
-        game_state.attempt_upgrade(wall_locations)
+        for location in self.default_wall_locations:
+            game_state.attempt_spawn(WALL, location)
+            
+        # Place supports behind turrets
+        support_locations = [[3, 11], [6, 11], [9, 11], [18, 11], [21, 11], [24, 11]]
+        game_state.attempt_spawn(SUPPORT, support_locations)
+        
+        # Upgrade walls so they soak more damage
+        # Prioritize upgrading walls that are frequently breached
+        priority_upgrade_locations = []
+        for location in self.scored_on_locations:
+            wall_loc = [location[0], location[1] + 1]
+            if wall_loc in self.wall_locations and wall_loc not in priority_upgrade_locations:
+                priority_upgrade_locations.append(wall_loc)
+        
+        game_state.attempt_upgrade(priority_upgrade_locations)
+        
+        # Then upgrade the rest of the walls if we have spare SP
+        other_walls = [loc for loc in self.wall_locations if loc not in priority_upgrade_locations]
+        game_state.attempt_upgrade(other_walls)
+        
+        # Upgrade turrets
+        game_state.attempt_upgrade(turret_locations)
 
     def build_reactive_defense(self, game_state):
         """
         This function builds reactive defenses based on where the enemy scored on us from.
-        We can track where the opponent scored by looking at events in action frames 
+        We can track where the opponent scored by looking at events in action frames
         as shown in the on_action_frame function
         """
         for location in self.scored_on_locations:
             # Build turret one space above so that it doesn't block our own edge spawn locations
             build_location = [location[0], location[1]+1]
             game_state.attempt_spawn(TURRET, build_location)
+
+    def improved_strategy(self, game_state):
+        """
+        An improved strategy that uses wall breach and health tracking
+        """
+        # First, repair any damaged walls
+        self.repair_walls(game_state)
+        
+        # Then build basic defenses
+        self.build_defences(game_state)
+        
+        # Build reactive defenses based on enemy attacks
+        self.build_reactive_defense(game_state)
+        
+        # In early game, focus on defense
+        if game_state.turn_number < 5:
+            self.stall_with_interceptors(game_state)
+        else:
+            # Analyze the enemy base
+            enemy_front_units = self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15])
+            
+            # If they have many units in the front, use demolishers
+            if enemy_front_units > 8:
+                self.demolisher_line_strategy(game_state)
+            else:
+                # Otherwise, send scouts to their weak points
+                # But only on odd turns to allow for resource buildup
+                if game_state.turn_number % 2 == 1:
+                    scout_spawn_location_options = [[13, 0], [14, 0]]
+                    best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
+                    scout_count = game_state.number_affordable(SCOUT)
+                    game_state.attempt_spawn(SCOUT, best_location, scout_count)
+                
+                # If we have resources remaining, strengthen our support structure
+                support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
+                game_state.attempt_spawn(SUPPORT, support_locations)
 
     def stall_with_interceptors(self, game_state):
         """
@@ -142,7 +246,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         # since we can't deploy units there.
         deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
         
-        # While we have remaining MP to spend lets send out interceptors randomly.
+        # While we have remaining MP to spend let's send out interceptors randomly.
         while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
             # Choose a random deploy location.
             deploy_index = random.randint(0, len(deploy_locations) - 1)
@@ -195,7 +299,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Now just return the location that takes the least damage
         return location_options[damages.index(min(damages))]
 
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
         total_units = 0
         for location in game_state.game_map:
             if game_state.contains_stationary_unit(location):
@@ -218,19 +322,29 @@ class AlgoStrategy(gamelib.AlgoCore):
         Processing the action frames is complicated so we only suggest it if you have time and experience.
         Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
         """
-        # Let's record at what position we get scored on
+        # Let's record at what position we get scored on and track wall breaches
         state = json.loads(turn_string)
         events = state["events"]
         breaches = events["breach"]
+        
+        # Reset wall_breaches for this new action frame
+        self.wall_breaches = []
+        
         for breach in breaches:
             location = breach[0]
             unit_owner_self = True if breach[4] == 1 else False
+            unit_type = breach[3]
+            
             # When parsing the frame data directly, 
             # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
             if not unit_owner_self:
                 gamelib.debug_write("Got scored on at: {}".format(location))
                 self.scored_on_locations.append(location)
-                gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
+                
+            # Check if the breach was a wall
+            if unit_type == WALL and unit_owner_self:
+                gamelib.debug_write("Wall breach at: {}".format(location))
+                self.wall_breaches.append(location)
 
 
 if __name__ == "__main__":
